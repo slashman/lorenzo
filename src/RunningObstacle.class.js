39e95e1f59010aa4d09c88c4e45e2d1b778f97d4
slashman <java.koder@gmail.com>
2016-07-18 19:53:29 -0500

    Try to enhance collision bounds with stage 2 obstacles

--- src/RunningObstacle.class.js
+++ src/RunningObstacle.class.js
@@ -4,7 +4,7 @@ function RunningObstacle(game, lorenzo, x, y, frames, group){
 	this.game = game;
 	this.lorenzo = lorenzo;
 	this.sprite = this.game.add.sprite(x, y, 'sprites', frames[0], group);
-	this.sprite.anchor.setTo(.5, 0);
+	this.sprite.anchor.setTo(.5, .5);
 	this.game.physics.arcade.enable(this.sprite);
 	this.sprite.animations.add('run', frames, 5, true);
 	this.sprite.animations.play('run');
@@ -16,13 +16,13 @@ RunningObstacle.prototype = {
 		if (this._react()){
 			this._doAI();	
 		}
-		if (this.sprite.y < 54) {
+		if (this.sprite.y < 54+7) {
 			this.sprite.body.velocity.y = 0;
-			this.sprite.y = 54;
+			this.sprite.y = 54+7;
 	    }
-	    if (this.sprite.y > 82){
+	    if (this.sprite.y > 82-7){
 	    	this.sprite.body.velocity.y = 0;
-			this.sprite.y = 82;	
+			this.sprite.y = 82-7;	
 	    }
 	},
 	_react: function(){
