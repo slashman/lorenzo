9ff361654869c18613ec6a28dc5e6aeced5680be
slashman <java.koder@gmail.com>
2016-07-19 10:17:49 -0500

    Move resources to /res

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ mkdir build/img
 mkdir build/wav
 cp html/* build
 cp jslib/* build
-cp png/* build/img
-cp wav/* build/wav
+cp res/png/* build/img
+cp res/wav/* build/wav
 browserify -t uglifyify src/LorenzoGame.js -o build/lorenzo.min.js
