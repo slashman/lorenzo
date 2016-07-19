13908948233fd07ada0b41443e5628c85f9759b7
slashman <java.koder@gmail.com>
2016-07-18 19:33:33 -0500

    Add Lorenza Sprites

--- src/Lorenza.js
+++ src/Lorenza.js
@@ -4,9 +4,9 @@ var Lorenza = {
 	init: function(lorenzoGame){
 		this.lorenzoGame = lorenzoGame;
 		this.game = lorenzoGame.game;
-		this.sprite = this.game.add.sprite(70, 57, 'sprites', 0);
-		this.sprite.animations.add('idle', [0, 1], 2, true); //TODO: Use Lorenza sprites
-		this.sprite.animations.add('die', [21, 22], 1, false);
+		this.sprite = this.game.add.sprite(70, 57, 'sprites', 33);
+		this.sprite.animations.add('idle', [33, 34], 2, true);
+		this.sprite.animations.add('die', [36, 37], 1, false);
 		this.sprite.animations.play('idle');
 	}, 
 	hit: function(){
